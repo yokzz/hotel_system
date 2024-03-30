00962556d8ba1a53959d6520fbf5a650cc2394ad
Zhelizko Daniel <yokzz.programming@gmail.com>
2024-03-30 23:01:39 +0200

    views added

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from booking.models import Room, Booking
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    # return HttpResponse("Some message :", status=400)
+    context = {
+        "render_string": "Hello, world!"
+    }
+    return render(request, template_name="booking/index.html", context=context)
